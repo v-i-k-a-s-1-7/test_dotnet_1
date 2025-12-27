431f5709701c52e1a7cf6b64ba790e6e01a6222b
v-i-k-a-s-1-7 <vikasrajak2003@gmail.com>
2025-12-27 14:33:15 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
